5eb15da5434537f5ab29b587174d69308bd7245d
Viacheslav <teodebz1998@gmail.com>
2020-01-09 12:21:08 +0200

    firstCommit

---
+++ CollectionAppAndersen/CollectionAppAndersen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionAppAndersen
+//
+//  Created by Вячеслав Савицкий on 09.01.2020.
+//  Copyright © 2020 Вячеслав Савицкий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
